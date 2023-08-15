1c438d2be77bc81f58e3ee668a670695b045d138
ctbus <ctbushman@gmail.com>
2023-08-15 13:10:03 -0400

    Actually add data.R

---
+++ data/data.R
@@ -0,0 +1,35 @@
+#' IgG
+#'
+#' @format ## `igg`
+#' A data frame:
+#' \describe{
+#'   \item{Taxa}{}
+#'   \item{SampleID}{}
+#'   \item{props}{}
+#'   \item{SubjectID}{}
+#'   \item{Analysis4}{}
+#'   \item{Population}{}
+#'   \item{Name}{}
+#'   \item{SNV}{}
+#'   \item{fraction}{}
+#'   ...
+#' }
+"igg"
+
+#' IgM
+#'
+#' @format ## `igm`
+#' A data frame:
+#' \describe{
+#'   \item{Taxa}{}
+#'   \item{SampleID}{}
+#'   \item{props}{}
+#'   \item{SubjectID}{}
+#'   \item{Analysis4}{}
+#'   \item{Population}{}
+#'   \item{Name}{}
+#'   \item{SNV}{}
+#'   \item{fraction}{}
+#'   ...
+#' }
+"igm"
