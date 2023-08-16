e2320e159118371636f0fadc39fccc803449ab7f
ctbus <ctbushman@gmail.com>
2023-08-16 11:32:49 -0400

    Add normalize_sum test

--- tests/testthat/test-mflow.R
+++ tests/testthat/test-mflow.R
@@ -2,6 +2,11 @@ dual_cts <- matrix(c(5, 7, 19, 13), nrow = 2)
 dual_fracs <- 35 * colSums(dual_cts)
 dual_props <- apply(dual_cts, 2, normalize_sum)
 
+test_that("normalize_sum works", {
+  vec <- c(25, 75)
+  expect_equal(normalize_sum(vec), c(0.25, 0.75))
+})
+
 test_that("mflow_probability works", {
   props <- c(0.0037825570, 0.0006065385, 0.0010901371, 0.0010616181)
   fracs <- c(0.146, 0.161, 0.237, 0.36)
